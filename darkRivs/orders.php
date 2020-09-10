cb9fa0c91cfc8d3e617445959cd237b7abb7d897
Valentine Gladyshko <valentine9022@gmail.com>
2020-09-10 12:09:50 +0300

    Fix details button in all orders table

--- darkRivs/orders.php
+++ darkRivs/orders.php
@@ -203,6 +203,20 @@ if (hash_equals($security_token, $security_token1)) {
                     [1, 'desc']
                 ]
             });
+            $('#allOrders tbody').on('click', 'td.details-control', function() {
+                var tr = $(this).closest('tr');
+                var row = allOrdersTable.row(tr);
+
+                if (row.child.isShown()) {
+                    // This row is already open - close it
+                    row.child.hide();
+                    tr.removeClass('shown');
+                } else {
+                    // Open this row
+                    row.child(format(row.data())).show();
+                    tr.addClass('shown');
+                }
+            });
             $('#allOrders').on('page.dt', function() {
                 $('.chevron-up').trigger('click');
             });
